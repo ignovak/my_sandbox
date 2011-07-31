3a53ded523053bdc693b42c520869ea8c876af23
Igor <bezengi@gmail.com>
2011-08-01 02:07:39 +0300

    aes

--- main.py
+++ main.py
@@ -1,115 +1,56 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import template, util
+#
 
-import os
-import logging
-import re
+from binascii import unhexlify
+from uuid import uuid4
 
+from Crypto.Cipher import AES
 
-RULES = {
-  'name': {
-     'required': {
-        'onerror': 'missingName'
-     },
-     'limit': {
-        'limit': 100,
-        'onerror': 'tooLongName'
-     }
-  },
-  'phone': {
-     'required': {
-        'onerror': 'missingPhone'
-     },
-     'type': {
-        'type': 'phone',
-        'onerror': 'incorrectPhone'
-     }
-  },
-  'email': {
-     'required': {
-        'onerror': 'missingName'
-     },
-     'type': {
-        'type': 'email',
-        'onerror': 'incorrectEmail'
-     }
-  }
-}
+from google.appengine.ext import webapp, db
+from google.appengine.ext.webapp import util, template
 
-def limit(val, args):
-  if len(val) > args['limit']:
-    return args['onerror']
+# from Crypto.PublicKey import RSA
+# from Crypto.PublicKey.RSA import Random
 
-def required(val, args):
-  if not val:
-    return args['onerror']
+class Game(db.Model):
+  passphrase = db.StringProperty()
 
-def type(val, args):
-  regexp = {
-    'phone': '^(\d{5,7}|(\(\d{3}\)|\d{3})\d{7}|\+\d{1,3}(\(\d{3}\)|\d{3})\d{7})$',
-    'email': '^[-.\w]+@(?:[a-z\d][-a-z\d]+\.)+[a-z]{2,6}$'
-  }[args['type']]
-  if not re.match(regexp, val):
-    return args['onerror']
+class Handler(webapp.RequestHandler):
+  def get(self, id):
+    record = Game.get_by_id(int(id))
+    passphrase = record.passphrase
+    cyphertext = self.request.query_string
 
-def bridge(rule, val, args):
-  f = {
-    'limit': limit,
-    'required': required,
-    'type': type
-  }[rule]
-  return f(val, args)
-
-
-class Main(webapp.RequestHandler):
-  def get(self):
-    path = os.path.join('index.html')
-    params = {
-
-        }
-
-    self.response.out.write(template.render(path, params))
-
-  def post(self):
-    if not self.__validateInput():
-      self.redirect('/error')
-      return
-
-    self.redirect('/ok')
-
-  def __validateInput(self):
-    for field in RULES.keys():
-      val = self.request.get(field)
-      for rule in RULES[field].keys():
-        # logging.info('field: %s, rule: %s, val: %s' % (field, rule, val))
-        # logging.info(RULES[field][rule])
-        error = bridge(rule, val, RULES[field][rule])
-        if error:
-          logging.info(error)
-          return False
-    return True
+    encobj = AES.new(passphrase)
+    plaintext = encobj.decrypt(unhexlify(cyphertext))
+    self.response.out.write(plaintext)
     
-class Others(webapp.RequestHandler):
+class NewHandler(webapp.RequestHandler):
   def get(self):
-    page = self.request.path[1:]
-    path = os.path.join('ok.html')
-    params = {
-        'label': 'Thank you!' if page == 'ok' else 'Error!'
-        }
+    passphrase = str(uuid4()).replace('-', '')
+    record = Game(passphrase=passphrase)
+    record.put()
+    self.response.out.write(record.key().id())
+    self.response.out.write('<br>')
+    self.response.out.write(passphrase)
 
-    try:
-      self.response.out.write(template.render(path, params))
-    except Exception:
-      pass
-    
+class OldHandler(webapp.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/plain'
+    self.response.out.write('It Works')
+    self.response.out.write('\n')
+    f = open('test.gpg')
+    self.response.out.write(f.read())
+    self.response.out.write('\n')
+    f = open('pub.key')
+    self.response.out.write(f.read())
 
 def main():
-  application = webapp.WSGIApplication([('/', Main), ('/.*', Others)],
-                                       debug=True)
+  application = webapp.WSGIApplication([
+    ('/new', NewHandler),
+    ('/(\d+)', Handler)
+    ], debug=True)
   util.run_wsgi_app(application)
 
 if __name__ == '__main__':
   main()
-
---
+++ validation.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template, util
+
+import os
+import logging
+import re
+
+
+RULES = {
+  'name': {
+     'required': {
+        'onerror': 'missingName'
+     },
+     'limit': {
+        'limit': 100,
+        'onerror': 'tooLongName'
+     }
+  },
+  'phone': {
+     'required': {
+        'onerror': 'missingPhone'
+     },
+     'type': {
+        'type': 'phone',
+        'onerror': 'incorrectPhone'
+     }
+  },
+  'email': {
+     'required': {
+        'onerror': 'missingName'
+     },
+     'type': {
+        'type': 'email',
+        'onerror': 'incorrectEmail'
+     }
+  }
+}
+
+def limit(val, args):
+  if len(val) > args['limit']:
+    return args['onerror']
+
+def required(val, args):
+  if not val:
+    return args['onerror']
+
+def type(val, args):
+  regexp = {
+    'phone': '^(\d{5,7}|(\(\d{3}\)|\d{3})\d{7}|\+\d{1,3}(\(\d{3}\)|\d{3})\d{7})$',
+    'email': '^[-.\w]+@(?:[a-z\d][-a-z\d]+\.)+[a-z]{2,6}$'
+  }[args['type']]
+  if not re.match(regexp, val):
+    return args['onerror']
+
+def bridge(rule, val, args):
+  f = {
+    'limit': limit,
+    'required': required,
+    'type': type
+  }[rule]
+  return f(val, args)
+
+
+class Main(webapp.RequestHandler):
+  def get(self):
+    path = os.path.join('index.html')
+    params = {
+
+        }
+
+    self.response.out.write(template.render(path, params))
+
+  def post(self):
+    if not self.__validateInput():
+      self.redirect('/error')
+      return
+
+    self.redirect('/ok')
+
+  def __validateInput(self):
+    for field in RULES.keys():
+      val = self.request.get(field)
+      for rule in RULES[field].keys():
+        # logging.info('field: %s, rule: %s, val: %s' % (field, rule, val))
+        # logging.info(RULES[field][rule])
+        error = bridge(rule, val, RULES[field][rule])
+        if error:
+          logging.info(error)
+          return False
+    return True
+    
+class Others(webapp.RequestHandler):
+  def get(self):
+    page = self.request.path[1:]
+    path = os.path.join('ok.html')
+    params = {
+        'label': 'Thank you!' if page == 'ok' else 'Error!'
+        }
+
+    try:
+      self.response.out.write(template.render(path, params))
+    except Exception:
+      pass
+    
+
+def main():
+  application = webapp.WSGIApplication([('/', Main), ('/.*', Others)],
+                                       debug=True)
+  util.run_wsgi_app(application)
+
+if __name__ == '__main__':
+  main()
+
